38c21026ec1a483002f9601b3583d0a11c069031
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2023-12-09 22:24:44 +0100

    new puzzle

---
+++ src/main/java/pl/bratek20/algorithms/puzzles/HexagonalMaze.java
@@ -0,0 +1,12 @@
+package pl.bratek20.algorithms.puzzles;
+
+import pl.bratek20.algorithms.common.puzzle.Puzzle;
+
+// https://www.codingame.com/ide/puzzle/hexagonal-maze
+public class HexagonalMaze extends Puzzle {
+
+    @Override
+    public void solve() {
+
+    }
+}
---
+++ src/test/java/pl/bratek20/algorithms/puzzles/HexagonalMazeTest.java
@@ -0,0 +1,39 @@
+package pl.bratek20.algorithms.puzzles;
+
+import pl.bratek20.algorithms.common.puzzle.Puzzle;
+import pl.bratek20.algorithms.common.puzzle.PuzzleTest;
+
+import java.util.List;
+
+class HexagonalMazeTest extends PuzzleTest {
+
+    @Override
+    protected Puzzle createSolution() {
+        return new HexagonalMaze();
+    }
+
+    @Override
+    protected List<TestData> testData() {
+        return List.of(
+                new TestData(
+                        """
+                                5 6
+                                #####
+                                #S#E#
+                                #_#_#
+                                #_#_#
+                                #___#
+                                #####
+                                """,
+                        """
+                                #####
+                                #S#E#
+                                #.#.#
+                                #.#.#
+                                #_..#
+                                #####
+                                """
+                )
+        );
+    }
+}
