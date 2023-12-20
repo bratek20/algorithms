efeec109b2fe94c4019d42fc7c2574c896873e82
Bartosz Rudzki (Legion) <bartosz.rudzki@tensquaregames.com>
2023-12-20 20:57:26 +0100

    extra logic, not passing CG tests

--- src/main/java/pl/bratek20/algorithms/puzzles/TileAFloor.java
+++ src/main/java/pl/bratek20/algorithms/puzzles/TileAFloor.java
@@ -1,5 +1,7 @@
 package pl.bratek20.algorithms.puzzles;
 
+import java.util.HashMap;
+import java.util.Map;
 import pl.bratek20.algorithms.common.array.d1.Array;
 import pl.bratek20.algorithms.common.array.d2.Array2DReader;
 import pl.bratek20.algorithms.common.array.d2.Array2DWriter;
@@ -8,15 +10,56 @@ import pl.bratek20.algorithms.common.puzzle.Puzzle;
 // https://www.codingame.com/ide/puzzle/tile-a-floor
 public class TileAFloor extends Puzzle {
 
+    //• () {} [] <> If any of these are on the left side, then show its opposite-mate on the right side
+    //• ^v AV wm WM un If any of these are on the top half, then show its opposite-mate on the bottom half
+    //• /\ If either of these is on the top left quadrant, then show its opposite-mate on the two adjacent quadrants
+    Map<Character, Character> leftRight = new HashMap<>();
+    Map<Character, Character> topBottom = new HashMap<>();
+    void fillMaps() {
+        leftRight.put('(', ')');
+        leftRight.put('{', '}');
+        leftRight.put('[', ']');
+        leftRight.put('<', '>');
+        leftRight.put('/', '\\');
+        leftRight.put('\\', '/');
+
+        topBottom.put('^', 'v');
+        topBottom.put('A', 'V');
+        topBottom.put('w', 'm');
+        topBottom.put('W', 'M');
+        topBottom.put('u', 'n');
+        topBottom.put('/', '\\');
+        topBottom.put('\\', '/');
+
+    }
+
     @Override
     public void solve() {
         var arr = Array2DReader.readCharSquare(in);
         int n = arr.getRows();
 
         var leftTop = arr;
-        var rightTop = arr.reverseColumns();
-        var leftBottom = arr.reverseRows();
-        var rightBottom = arr.reverseRows().reverseColumns();
+        var rightTop = arr.reverseColumns().map(c -> {
+            var value = c.getValue();
+            if (leftRight.containsKey(value)) {
+                return leftRight.get(value);
+            }
+            return value;
+        });
+        var leftBottom = arr.reverseRows().map(c -> {
+            var value = c.getValue();
+            if (topBottom.containsKey(value)) {
+                return topBottom.get(value);
+            }
+            return value;
+        });
+        var rightBottom = rightTop.reverseRows().map(c -> {
+            var value = c.getValue();
+            if (topBottom.containsKey(value)) {
+                return topBottom.get(value);
+            }
+            return value;
+        });
 
         var top = leftTop.concatByColumn(rightTop);
         var bottom = leftBottom.concatByColumn(rightBottom);
