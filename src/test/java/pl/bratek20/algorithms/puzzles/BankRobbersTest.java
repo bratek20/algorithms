15ad218e4c9de885e3264e3c7e4981238cefc06d
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2023-12-22 16:38:03 +0100

    not passing test

--- src/test/java/pl/bratek20/algorithms/puzzles/BankRobbersTest.java
+++ src/test/java/pl/bratek20/algorithms/puzzles/BankRobbersTest.java
@@ -46,6 +46,35 @@ class BankRobbersTest extends PuzzleTest {
                 """
                 10
                 """
+            ),
+            new TestData("big heist",
+                """
+                    5
+                    20
+                    6 3
+                    7 1
+                    4 4
+                    8 4
+                    3 0
+                    4 3
+                    8 1
+                    3 3
+                    5 5
+                    7 6
+                    6 2
+                    5 3
+                    5 4
+                    7 0
+                    7 0
+                    8 4
+                    6 0
+                    6 5
+                    3 2
+                    4 2
+                    """,
+                """
+                    6515625
+                    """
             )
         );
     }
