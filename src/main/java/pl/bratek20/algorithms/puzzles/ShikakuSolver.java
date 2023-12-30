8c3960fe200defe628a4a665d6ad47ff792812bb
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2023-12-30 15:18:16 +0100

    new puzzle

---
+++ src/main/java/pl/bratek20/algorithms/puzzles/ShikakuSolver.java
@@ -0,0 +1,11 @@
+package pl.bratek20.algorithms.puzzles;
+
+import pl.bratek20.algorithms.common.puzzle.Puzzle;
+
+// https://www.codingame.com/ide/puzzle/shikaku-solver
+public class ShikakuSolver extends Puzzle {
+    @Override
+    public void solve() {
+        //TODO
+    }
+}
---
+++ src/test/java/pl/bratek20/algorithms/puzzles/ShikakuSolverTest.java
@@ -0,0 +1,48 @@
+package pl.bratek20.algorithms.puzzles;
+
+import pl.bratek20.algorithms.common.puzzle.Puzzle;
+import pl.bratek20.algorithms.common.puzzle.PuzzleTest;
+
+import java.util.List;
+
+class ShikakuSolverTest extends PuzzleTest {
+
+    @Override
+    protected Puzzle createSolution() {
+        return new ShikakuSolver();
+    }
+
+    @Override
+    protected List<TestData> testData() {
+        return List.of(
+            new TestData("example",
+                """
+                    10 10
+                    0 0 0 0 0 0 0 0 9 0
+                    0 0 0 0 0 0 9 0 0 0
+                    0 0 0 0 0 0 0 0 0 0
+                    0 20 0 0 8 0 0 0 6 0
+                    0 0 0 0 0 0 0 0 0 0
+                    0 0 0 6 0 0 6 0 0 0
+                    10 0 0 0 0 0 0 0 0 0
+                    0 0 0 0 0 0 0 0 0 0
+                    0 0 6 0 6 0 0 0 8 0
+                    0 0 0 0 0 0 6 0 0 0
+                    """,
+                """
+                    1
+                    AAAABBBCCC
+                    AAAABBBCCC
+                    AAAABBBCCC
+                    AAAADDDDEE
+                    AAAADDDDEE
+                    FFGGGHHHEE
+                    FFGGGHHHII
+                    FFJJKKLLII
+                    FFJJKKLLII
+                    FFJJKKLLII
+                    """
+            )
+        );
+    }
+}
